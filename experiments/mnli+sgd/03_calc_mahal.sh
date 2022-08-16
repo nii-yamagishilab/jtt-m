e712880c1b53afd42ddd99846f0e1a6502b6a354
canasai <canasai@gmail.com>
2022-08-16 12:35:59 +0900

    Use symbolic link for same file

--- experiments/mnli+sgd/03_calc_mahal.sh
+++ experiments/mnli+sgd/03_calc_mahal.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-set -ex
-
-base_dir="$(basename "$PWD")"
-
-# Remove suffix until the "+" character
-data_name="${base_dir%+*}"
-data_dir="../data/${data_name}"
-
-pretrained='bert-base-uncased'
-max_len=128
-out_dir="${pretrained}-${max_len}-out"
-
-python '../../mahalanobis_scorer.py' \
-  --gold_file "${data_dir}"/train.jsonl \
-  --emb_file "${out_dir}"/train.emb.npy
+../fever+sgd/03_calc_mahal.sh
